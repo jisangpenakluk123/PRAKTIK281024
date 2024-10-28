a3290259059fa8997b632f5137744b17a4850600
panji <sample@example.com>
2024-10-28 13:52:35 +0700

    git commit  pada stage filepanji

---
+++ belajar_subprogram.py
@@ -0,0 +1,6 @@
+def mencari_luas_lingkaran(jari-jari):
+    luas_lingkaran = 3.14 *jari-jari*jari-jari
+    return luas_lingkaran 
+
+lingkaran_pertama=mencari_luas_lingkungan(5)
+print(lingkungan_pertama)
--- run_shell.sh
+++ run_shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "---baris ini adalah shell script yang  di jalankan---"
-python3 hallo.py
+echo "---baris ini adalah shell scriptnya PANJI  yang  di jalankan---"
+python3 hallo.py belajar_subprogram.py
 
